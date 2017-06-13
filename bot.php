9cd0010d2e1d3c5ca1f83ae8ebd4b168ad1a9b92
flukel <hutsanai_s@cmu.ac.th>
2017-06-13 16:13:30 +0700

    fix minor bug
    fix var line 22

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
             // Build message to reply back
             $messages = [
                 'type' => 'text',
-                'text' => 'ครับ' + $message + 'อ้วนครับ'
+                'text' => 'ครับ' + $text + 'อ้วนครับ'
                   
             ];
 
