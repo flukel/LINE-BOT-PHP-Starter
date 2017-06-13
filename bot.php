ea5066810b3e8c441d895faea5aca8d7b37d00b1
flukel <hutsanai_s@cmu.ac.th>
2017-06-13 11:40:46 +0700

    try add image reply on bot again
    blank text message and add image url again

--- bot.php
+++ bot.php
@@ -18,8 +18,11 @@ if (!is_null($events['events'])) {
 
             // Build message to reply back
             $messages = [
-                'type' => 'text',
-                'text' => 'ครับ ไปละครับ'
+                //'type' => 'text',
+                //'text' => 'ครับ'
+                    "type": "image",
+                    "originalContentUrl": "http://nerdist.com/wp-content/uploads/2016/05/20160506_nerdistnews_capexclusive_1x1.jpg",
+                    "previewImageUrl": "http://nerdist.com/wp-content/uploads/2016/05/20160506_nerdistnews_capexclusive_1x1.jpg"
             ];
 
             // Make a POST Request to Messaging API to reply to sender
