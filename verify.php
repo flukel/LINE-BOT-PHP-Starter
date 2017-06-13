23d4b023f5b8b3374c02a1f6fd08e78d42f51012
flukel <hutsanai_s@cmu.ac.th>
2017-06-13 11:11:16 +0700

    fix verify.php
    fix some bug on verify.php

--- verify.php
+++ verify.php
@@ -3,9 +3,9 @@
  * @Author: fluk
  * @Date:   2017-06-13 05:07:23
  * @Last Modified by:   fluk
- * @Last Modified time: 2017-06-13 05:08:04
+ * @Last Modified time: 2017-06-13 05:10:51
  */
-<?php
+
 $access_token = 'LUzOBN4LJxC2mvmKX0CO7+XHdL5Lrgw/Lnkwz8uCJTXGZdMGuYzqODXob4IISZQCRciewurTGWjj+Vy2+06mSPNpkJKD/nrqC3q5meNDFaPa5sIpl1p8slrThgdUgWw+lQEK+pR6mzD5LQPR+gR9/AdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
