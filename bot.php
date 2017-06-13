d6acd9a52fa7b0b2d11fedf4058cef79a40c6bd3
flukel <hutsanai_s@cmu.ac.th>
2017-06-13 11:32:51 +0700

    try add image url
    add image url on bot.php

--- bot.php
+++ bot.php
@@ -18,8 +18,9 @@ if (!is_null($events['events'])) {
 
             // Build message to reply back
             $messages = [
-                'type' => 'text',
-                'text' => 'ว่าใดครับ'
+                'type' => 'image',
+                'originalContentUrl' => 'http://nerdist.com/wp-content/uploads/2016/05/20160506_nerdistnews_capexclusive_1x1.jpg'
+                'previewImageUrl' => 'http://nerdist.com/wp-content/uploads/2016/05/20160506_nerdistnews_capexclusive_1x1.jpg'
             ];
 
             // Make a POST Request to Messaging API to reply to sender
